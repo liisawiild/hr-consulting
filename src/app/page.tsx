f34736bcf9b02d3d29ad37cd274b02946e4afee5
Lisa Wild <lisakwild@gmail.com>
2025-01-26 16:14:06 -0800

    corrected above the fold screen height, created logo strip along the bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Newsletter from '@/components/newsletter/Newsletter';
 import Motto from '@/components/motto/Motto';
-import { prisma } from '../../lib/prisma'
+import Logos from '@/components/logos/Logos';
+import { prisma } from '../../lib/prisma';
 
 
 export default async function Home() {
@@ -11,10 +12,14 @@ export default async function Home() {
     }
   })
 
-   return (
-    <div className="w-full h-screen">
+  return (
+    <div className="w-full min-h-[calc(100vh-72px)] relative bg-white">
       <Motto />
-      <Newsletter user={user}/>
+      <Newsletter user={user} />
+      <div className="absolute bottom-0">
+        <Logos />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/logos/Logos.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Logos() {
+
+  return (
+    <div className="flex flex-row justify-between items-center ml-5 bg-white">
+      <div className="font-bold text-xl text-black px-10">
+        LOGO 1
+      </div>
+      <div className="font-bold text-xl text-black px-10">
+        LOGO 2
+      </div>
+      <div className="font-bold text-xl text-black px-10">
+        LOGO 3
+      </div>
+
+
+    </div>
+
+  );
+}
